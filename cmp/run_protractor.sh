f8c84fff80295d62c3b861689c079657a4ee1617
Igor Shekhtman <ishekhtman@apixio.com>
2015-06-24 08:24:33 -0700

    adding hcc and cmp test suite execution options to the page

--- cmp/run_protractor.sh
+++ cmp/run_protractor.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export DISPLAY=:1;
 export LD_LIBRARY_PATH=/opt/google/chrome/lib
-cd /home/protractor/minimal
+cd /home/protractor/cmp
 sudo -u protractor DISPLAY=:1 LD_LIBRARY_PATH=/opt/google/chrome/lib protractor protractor.config.js
