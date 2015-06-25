b3a8bca30121b6a044b68347b647f6694c2fe24b
Igor Shekhtman <ishekhtman@apixio.com>
2015-06-25 08:45:00 -0700

    working on Protractor based Apixio Test framework

--- pr/testspecs/01empty.spec.js
+++ pr/testspecs/01empty.spec.js
@@ -5,8 +5,8 @@
 
 //var login = require('../pages/login'),
 //    mock = require("../mock-helper"),
-//var verbose = true,
-//    lastLogs = [],
+var verbose = true,
+    lastLogs = [];
 //    expect = require('chai').use(require('chai-as-promised')).expect;
 
 console.log("progress report test spec");
