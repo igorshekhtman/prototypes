c051d4b6f9b93c9bc6065f7d3a27a36fa42b99ba
Igor Shekhtman <ishekhtman@apixio.com>
2015-06-25 15:44:37 -0700

    resuming work on Apixio Protractor Test framework

--- pr/testspecs/01login.spec.js
+++ pr/testspecs/01login.spec.js
@@ -56,6 +56,7 @@ describe('Apixio HCC homepage', function() {
     //var pwlabel = 
     
     expect(browser.driver.getTitle()).toEqual('Apixio HCC Optimizer');
+    expect(browser.driver.getTitle()).toEqual('Apixio HCC Optimizer');
     
     
     var user_field = element(by.css('input[name="username"]'));
