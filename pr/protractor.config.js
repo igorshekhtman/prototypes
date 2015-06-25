0e69adad217b87d7ad8e8125bc1f0e6a67e66978
Igor Shekhtman <ishekhtman@apixio.com>
2015-06-25 10:12:48 -0700

    working on Protractor based Apixio Test framework

--- pr/protractor.config.js
+++ pr/protractor.config.js
@@ -2,11 +2,11 @@ exports.config={
 	capabilities: {
 		'browserName': 'chrome'
 	},
-	mochaOpts:{
-		reporter:'spec',
-		enableTimeouts:false
-	},
-	framework:'mocha',
+	//mochaOpts:{
+	//	reporter:'spec',
+	//	enableTimeouts:false
+	//},
+	//framework:'mocha',
 	//specs:['empty.spec.js']
 	specs:['testspecs/*.spec.js']
 };
