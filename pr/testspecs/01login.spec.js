650a58236662d16bec3c99e0487d53b7eda341a7
Igor Shekhtman <ishekhtman@apixio.com>
2015-06-25 11:22:51 -0700

    working on Protractor based Apixio Test framework

--- pr/testspecs/01login.spec.js
+++ pr/testspecs/01login.spec.js
@@ -5,28 +5,28 @@
  
 //console.log("progress report test spec - Igor");
 
-describe('Apixio HCC homepage', function() {
-
-  it('should open HCC application', function() {
-  //  browser.get('google.com');
-    browser.driver.get('https://hccstage2.apixio.com/account/login/');
-    
-    console.log("progress report test spec - Igor");    
-
-
-  });
-
-});
-
-
 //describe('Apixio HCC homepage', function() {
+
 //  it('should open HCC application', function() {
-//    browser.get('https://hccstage2.apixio.com/account/login/');
+  //  browser.get('google.com');
+//    browser.driver.get('https://hccstage2.apixio.com/account/login/');
+    
+//    console.log("progress report test spec - Igor");    
 
-    //element(by.model('yourName')).sendKeys('Julie');
 
-    //var greeting = element(by.binding('yourName'));
-
-    //expect(greeting.getText()).toEqual('Hello Julie!');
 //  });
+
 //});
+
+
+describe('Apixio HCC homepage', function() {
+  it('should open HCC application', function() {
+    browser.driver.get('https://hccstage2.apixio.com/account/login/');
+
+    element(by.model('yourName')).sendKeys('Julie');
+
+    var greeting = element(by.binding('yourName'));
+
+    expect(greeting.getText()).toEqual('Hello Julie!');
+  });
+});
