a3df7a48a35be582e131072b5b8306b47fe2ef40
Igor Shekhtman <ishekhtman@apixio.com>
2015-06-25 16:16:54 -0700

    resuming work on Apixio Protractor Test framework

--- pr/protractor.config.js
+++ pr/protractor.config.js
@@ -4,25 +4,12 @@ exports.config={
 	},
 	
 	onPrepare: function() {
-    // 1920x1200
-    // 1280x1024
-    // var width = 1920;
-    // var height = 1200;
-     
-     //global.isAngularSite = function(flag){
-     //       browser.ignoreSynchronization = !flag;
-     //};
+
       
     
      browser.ignoreSynchronization = true;
-    // browser.driver.manage().window().setSize(width, height);
-     
-     browser.driver.manage().window().maximize();
-     
-     browser.getCapabilities().then(function (cap) {
-  	 browser.browserName = cap.caps_.browserName;
-  	 browser.browserVersion = cap.caps_.version;
-	 });
+
+	 };
 	
 	//mochaOpts:{
 	//	reporter:'spec',
