4102d793d6785095ddfbd25aa07ab0054d148b6b
Igor Shekhtman <ishekhtman@apixio.com>
2015-06-25 09:42:02 -0700

    working on Protractor based Apixio Test framework

--- pr/testspecs/01login.spec.js
+++ pr/testspecs/01login.spec.js
@@ -4,3 +4,14 @@
  
  
 console.log("progress report test spec");
+describe('angularjs homepage', function() {
+  it('should greet the named user', function() {
+    browser.get('http://www.angularjs.org');
+
+    element(by.model('yourName')).sendKeys('Julie');
+
+    var greeting = element(by.binding('yourName'));
+
+    expect(greeting.getText()).toEqual('Hello Julie!');
+  });
+});
