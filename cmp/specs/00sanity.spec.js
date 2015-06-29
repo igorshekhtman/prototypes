26a589aad683d04239037ab672170119d4bb25a9
Igor Shekhtman <ishekhtman@apixio.com>
2015-06-29 14:57:17 -0700

    Continuing work on Protractor NodeJS Apixio Test framework

--- cmp/specs/00sanity.spec.js
+++ cmp/specs/00sanity.spec.js
@@ -6,7 +6,7 @@ describe("Sanity Test to ensure the correctness of protractor tests",function(){
     it("Should be able to see the login page",function(){
         browser.get("https://portal-stg.apixio.com").then(function(){
             browser.getTitle().then(function(title){
-               expect(title).to.be.equal("Apixio Login Page");
+               expect(title).to.be.equal("Apixio Login Pag");
             });
         });
     });
