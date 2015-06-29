ec1b0e4b75a04af8b8208b3b65c8618c6eb166a1
Igor Shekhtman <ishekhtman@apixio.com>
2015-06-29 11:09:16 -0700

    Continuing work on Protractor NodeJS Apixio Test framework

--- cmp/protractor.config.js
+++ cmp/protractor.config.js
@@ -1,9 +1,25 @@
 exports.config={
 	capabilities: {
-		'browserName': 'chrome'
+		browserName: 'chrome'
 	},
-	framework : 'mocha',
-	
-	
-	specs:['empty.spec.js']
+
+	onPrepare: function() {
+
+
+
+		browser.ignoreSynchronization = true;
+		browser.driver.manage().window().maximize();
+		browser.getCapabilities().then(function (cap) {
+			browser.browserName = cap.caps_.browserName;
+			browser.browserVersion = cap.caps_.version;
+		});
+
+	},
+
+	mochaOpts:{
+		reporter:'spec',
+		enableTimeouts:false
+	},
+	framework:'mocha',
+	specs:['specs/*.spec.js']
 };
